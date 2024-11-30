d0fc7301b292a1ad80c92074c4c490fc76b0fcb9
Mohammad Mohammad <it.msm88@gmail.com>
2024-11-30 15:42:08 +0100

    initialize empty project

---
+++ Employee-Rest-API-Project/cruddemo/cruddemo/src/test/java/com/moha/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moha.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
