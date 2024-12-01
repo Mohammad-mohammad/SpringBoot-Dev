c9bf446bb2f23d87dfda38c9ee693a27cf3c7c8d
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-01 20:06:32 +0100

    spring data rest config and sorting

--- Employee-Rest-API-Project/cruddemo/cruddemo/src/main/java/com/moha/cruddemo/dao/EmployeeRepository.java
+++ Employee-Rest-API-Project/cruddemo/cruddemo/src/main/java/com/moha/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.moha.cruddemo.dao;
 
 import com.moha.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// to change the endpoint from ../employees to ../members
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
