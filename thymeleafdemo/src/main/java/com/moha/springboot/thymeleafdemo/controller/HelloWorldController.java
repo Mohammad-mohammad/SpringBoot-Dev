0a6ef76a049842929851c6c709e63c62256149ed
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-03 16:20:56 +0100

    soring mvc form and adding data to MVC model

--- thymeleafdemo/src/main/java/com/moha/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/moha/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.moha.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,14 @@ public class HelloWorldController {
     // need a new controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDuds(HttpServletRequest request, Model theModel){
+        String studentName = request.getParameter("studentName");
+        String result = "Yo! "+ studentName.toUpperCase();
+        theModel.addAttribute("message", result);
         return "helloworld";
     }
 }
