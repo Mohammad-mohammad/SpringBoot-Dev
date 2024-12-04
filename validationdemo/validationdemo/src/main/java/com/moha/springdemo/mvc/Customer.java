2cdebd0d047fefcd82b9e9e37c6d5c6dbbb59cec
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-04 20:21:27 +0100

    required integer value

--- validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/Customer.java
+++ validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/Customer.java
@@ -10,9 +10,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName="";
 
+    @NotNull(message = "is required")
     @Min(value = 0,message = "must be grater or equal to zero")
     @Max(value = 10, message = "must be less or equal to ten")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -25,11 +26,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
