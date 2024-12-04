ca36143dfbd18c7ebb31a6a178f75fc769340555
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-04 20:16:01 +0100

    regular expression validation

--- validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/Customer.java
+++ validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.moha.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -16,6 +13,17 @@ public class Customer {
     @Min(value = 0,message = "must be grater or equal to zero")
     @Max(value = 10, message = "must be less or equal to ten")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
