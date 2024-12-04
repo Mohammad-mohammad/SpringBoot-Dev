de94a09380d32abf7c63abd5bb1b19a5edb50654
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-04 12:17:20 +0100

    spring mvc data binding - drop-down list -1-

--- thymeleafdemo/src/main/java/com/moha/springboot/thymeleafdemo/model/Student.java
+++ thymeleafdemo/src/main/java/com/moha/springboot/thymeleafdemo/model/Student.java
@@ -4,6 +4,7 @@ public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student() {
     }
@@ -29,11 +30,20 @@ public class Student {
         this.lastName = lastName;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 }
