1d8b0ce47d72e0b312aec6998b7abca4d72fbbdf
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-04 19:23:09 +0100

    required field - dealing with whitespace validation using initBinder

--- validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/CustomerController.java
+++ validationdemo/validationdemo/src/main/java/com/moha/springdemo/mvc/CustomerController.java
@@ -1,15 +1,29 @@
 package com.moha.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // Add an initBinder to convert trim input strings
+    // Remove leading and trailing whitespace
+    // Resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/showForm")
     public String showForm(Model model){
@@ -25,6 +39,7 @@ public class CustomerController {
             BindingResult bindingResult
         ){
 
+        System.out.println("Last Name: |"+customer.getLastName()+"|");
         if(bindingResult.hasErrors()){
             return "customer-form";
         }else {
