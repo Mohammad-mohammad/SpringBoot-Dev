4f0c065e43cd89ce8c09da26217746fda4ea9795
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-05 21:24:12 +0100

    start wit old project - JDBC auth

---
+++ spring-mvc-security-jdbc/src/test/java/com/moha/springdemo/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moha.springdemo.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
