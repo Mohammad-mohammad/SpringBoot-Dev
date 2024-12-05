783569e25f0c4a186a35d6e2ac664febfb97a6cd
Mohammad Mohammad <it.msm88@gmail.com>
2024-12-05 18:17:11 +0100

    creating empty home page

---
+++ demosecurity/demosecurity/src/main/java/com/moha/springdemo/demosecurity/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.moha.springdemo.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+}
